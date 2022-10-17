9a830a07137b74ce22ee110b54d3b074ab089d0f
brijeshmaster <brijeshkumarmaisuriya@gmail.com>
2022-10-17 15:23:48 +0200

    initialcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import About from './component/About'
+import Nav from './component/Nav'
+import TextForm from './component/TextForm'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <Nav />
+      <div className="container my-5">
+
+      <TextForm heading='Enter The Text To Analyze'/>
+      </div>
+      <About />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/component/TextForm.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+
+const TextForm = (props) => {
+
+    const [mytext, setmytext] = useState('enter the text')
+
+
+    const clickme = () => {
+        const newtext =mytext.toUpperCase()
+       setmytext(newtext) 
+    }
+
+    const cliclowkme = () => {
+        const newtext =mytext.toLowerCase()
+       setmytext(newtext) 
+    }
+
+    const changeme = (e) => { 
+       setmytext( e.target.value)
+    }
+
+  return (<>
+<div className='container'>
+ <h1>{props.heading}</h1>  
+<div className="mb-3">
+  <textarea className="form-control" value={mytext} onChange={changeme} id="mybox" rows="14"></textarea>
+</div>
+<button className="btn btn-primary mx-2" onClick={clickme}>conver to uppercase</button>
+<button className="btn btn-primary mx-2" onClick={cliclowkme}>conver to lowercase</button>
+    </div>
+    <div className="container my-4">
+        <h1>your text summary</h1>
+        <p>{mytext.split(" ").length} words and {mytext.length} characters</p>
+        <p>preview</p>
+        <p>{mytext}</p>
+    </div>
+  </>
+  )
+}
+
+export default TextForm
